d31fcfcac5db856d7a7e2189690feaade9e6bc4a
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-05 21:45:30 -0600

    Listando empleados

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { ListaEmpleado } from "@/components/ListaEmpleado";
 import Image from "next/image";
 
 export default function Home() {
   return (
       <div className="max-w-7xl">
-        
+        <ListaEmpleado />
       </div>
   );
 }
--- components/ListaEmpleado.tsx
+++ components/ListaEmpleado.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+'use client'
+import { Empleado } from '@/Interfaces/Empleado'
+import { appsettings } from '@/settings/appsettings';
+import { use, useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
 
 export const ListaEmpleado = () => {
+  const [empleados, setEmpleados] = useState<Empleado[]>([]);
+
+  const obtenerEmpleados = async () =>{
+    const response = await fetch(`${appsettings.apiUrl}Empleado`);
+    if(response.ok){
+      const data = await response.json();
+      console.log(data)
+      setEmpleados(data)
+    }
+  }
+
+  useEffect(()=>{
+    obtenerEmpleados()
+  },[])
+
   return (
     <div></div>
   )
