28ebfe1ca0a565482ea2d7e563316b0308ec899f
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-05 22:26:52 -0600

    Menu
    Navigation

---
+++ app/departamento/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Departamento() {
+  return (
+    <div>departamento</div>
+  )
+}
+
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client"
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faBars, faXmark} from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
@@ -15,14 +15,14 @@ export const Navbar = () => {
 
     return (
         <nav className="w-full bg-[#0D0D0D] fixed top-0 left-0 right-0 z-10 py-2">
-            <div className="justify-between px-4 mx-auto lg:max-w-7xl lg:flex lg:items-center  lg:px-8">
+            <div className="justify-between px-2 mx-auto lg:max-w-7xl lg:flex lg:items-center  lg:px-6">
                 <div>
                     <div className="flex items-center justify-between py-1 lg:py-5 lg:block">
                         <Link href="/">
-                            <h1 className={`px-2 bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-rose-400 text-2xl font-bold underline underline-offset-8 decoration-rose-500 decoration-1`} aria-label="Jeimmy Lopez Vega">JeimmyLV</h1>
+                            <h1 className={`px-2 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500 text-2xl font-bold underline underline-offset-8 decoration-teal-500 decoration-1`}>Api</h1>
                         </Link>
                         <div className="block aspect-square lg:hidden">
-                            <button className="p-2 text-white outline-none text-3xl focus:border focus:border-rose-600" aria-label='Open or Close Navbar'
+                            <button className="p-2 text-white outline-none text-3xl focus:border focus:border-teal-600" aria-label='Open or Close Navbar'
                                 onClick={() => setNavbar(!navbar)}>
                                 {navbar ? (
                                     <FontAwesomeIcon icon={faXmark} className='text-white' title='Close Menu' />
@@ -36,29 +36,14 @@ export const Navbar = () => {
                 <div>
                     <div className={`pb-2 overflow-y-scroll lg:overflow-y-visible lg:block lg:pb-0 lg:mt-0 ${navbar ? 'p-2 lg:p-0 block' : 'hidden'}`}>
                         <ul className="h-[82vh] items-center justify-center overflow-y-scroll md:h-screen lg:overflow-y-visible lg:h-auto lg:flex lg:gap-1">
-                            <li className={"py-3 text-base text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-rose-600 hover:underline hover:underline-offset-8 lg:py-1 lg:px-4 lg:font-medium lg:text-lg lg:border-b-0"}>
-                                <Link href="/" onClick={() => setNavbar(!navbar)} className={pathname == '/' ? 'text-rose-600' : ''}>
-                                    Home
+                            <li className={"py-3 text-base text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-teal-400 hover:underline hover:underline-offset-8 lg:py-1 lg:px-4 lg:font-medium lg:text-lg lg:border-b-0"}>
+                                <Link href="/" onClick={() => setNavbar(!navbar)} className={pathname == '/' ? 'text-teal-400 font-bold' : ''}>
+                                    Empleados
                                 </Link>
                             </li>
-                            <li className="py-3 text-base text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-rose-600 hover:underline hover:underline-offset-8 lg:py-1 lg:px-4 lg:font-medium lg:text-lg lg:border-b-0">
-                                <Link href="/projects" onClick={() => setNavbar(!navbar)} className={pathname == '/projects' ? 'text-rose-600' : ''}>
-                                    Projects
-                                </Link>
-                            </li>
-                            <li className={`py-3 text-base text-white text-start border-b-2 border-white border-opacity-20 pl-0 lg:py-1 lg:px-4 lg:font-medium lg:text-lg lg:border-2 lg:text-white lg:border-rose-600 lg:rounded-xl lg:ml-2 lg:hover:bg-rose-600 ${pathname == '/contact' ? 'text-rose-600 lg:bg-rose-600 lg:text-black' : ''}`}>
-                                <Link href="/contact" onClick={() => setNavbar(!navbar)} >
-                                    Contact
-                                </Link>
-                            </li>
-                            <li className="py-3 text-2xl text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-rose-600 lg:py-1 lg:pl-4 lg:border-b-0 lg:hover:scale-125">
-                                <Link href="https://github.com/Jeim05/" title="Visit my GitHub profile" onClick={() => setNavbar(!navbar)} target='_blank'>
-                                    <FontAwesomeIcon icon={faGithub} className='aspect-square w-6 h-6' />
-                                </Link>
-                            </li>
-                            <li className="py-3 text-2xl text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-rose-600 lg:py-1 lg:px-2 lg:border-b-0 lg:hover:scale-125">
-                                <Link href="https://www.linkedin.com/in/jeimmylopezvega05/" title="Contact me by LinkedIn" onClick={() => setNavbar(!navbar)} target='_blank'>
-                                    <FontAwesomeIcon icon={faLinkedin} className='aspect-square w-6 h-6' />
+                            <li className="py-3 text-base text-white text-start border-b-2 border-white border-opacity-20 pl-0 hover:text-teal-400 hover:underline hover:underline-offset-8 lg:py-1 lg:px-4 lg:font-medium lg:text-lg lg:border-b-0">
+                                <Link href="/departamento" onClick={() => setNavbar(!navbar)} className={pathname == '/departamento' ? 'text-teal-400 font-bold' : ''}>
+                                    Departamentos
                                 </Link>
                             </li>
                         </ul>
