1d204a8713cc70e534dad0cc1f286c2b20466f9d
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-06 22:31:31 -0600

    Estrucrando botones y diseño

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import { ListaEmpleado } from "@/components/ListaEmpleado";
 
 export default function Home() {
   return (
-    <div className="container max-w-screen-xl mx-auto py-8 px-2">
+    <div className="container max-w-screen-xl mx-auto py-10 px-2">
+      <div className="py-10 lg:pt-16 my-5">
+        <h1 className="py-2 font-bold text-xl lg:text-5xl">Frontend Api Empleados</h1>
+        <p className="py-1 text-lg">
+          Aplicación donde se obtienen los datos de una api que contiene datos de empleados y
+          los departamentos para el cual trabajan
+        </p>
+      </div>
       <ListaEmpleado />
     </div>
   );
--- components/ListaEmpleado.tsx
+++ components/ListaEmpleado.tsx
@@ -23,50 +23,50 @@ export const ListaEmpleado = () => {
   }, [])
 
   return (
-    <div className='container mx-auto py-10'>
-      <div className='card py-8'>
+    <div className='container mx-auto'>
+      <div className='card'>
         <div className='card-header'>
-          <h1 className='text-3xl font-bold'>Lista de empleados</h1>
-          <div className='my-5'>
-            <Link href="/" className='py-4 px-2 bg-blue-700 text-white text-base font-semibold rounded-md'>Crear nuevo</Link>
+          <div className='grid grid-cols-2'>
+            <h1 className='text-4xl font-bold'>Lista de empleados</h1>
+            <div className='my-5 text-end'>
+              <Link href="/" className='py-3 px-2 bg-blue-700 text-white text-base font-semibold rounded-md'>Crear nuevo</Link>
+            </div>
           </div>
         </div>
-      </div>
-      <hr />
-      <div className='card-body'>
-        <table className="tabla">
-          <thead >
-            <tr>
-              <th >Nombre completo</th>
-              <th >Fecha de contrato</th>
-              <th>Departamento</th>
-              <th>Sueldo</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              empleados.map((item) => (
-                <tr key={item.idEmpleado} className="">
-                  <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>{item.nombreCompleto}</td>
-                  <td className='px-6 py-4'>{item.fechaContrato}</td>
-                  <td className='px-6 py-4'>{item.departamento.nombre}</td>
-                  <td className='px-6 py-4'>{item.sueldo}</td>
-                  <td className='px-6 py-4 flex gap-2 justify-start'>
-                    <Link href="/">
-                      <Image src="/images/pen-to-square-solid.svg" width={25} height={25} alt='edit' title='close' className='fill-yellow-500' />
-                    </Link>
-                    <button className=''>
-                      <Image src="/images/trash-can-solid.svg" width={25} height={25} alt='edit' title='close' className='fill-yellow-500' />
-                    </button>
-                  </td>
-                </tr>
+        <div className='card-body'>
+          <table className="tabla">
+            <thead >
+              <tr>
+                <th >Nombre completo</th>
+                <th >Fecha de contrato</th>
+                <th>Departamento</th>
+                <th>Sueldo</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                empleados.map((item) => (
+                  <tr key={item.idEmpleado} className="">
+                    <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>{item.nombreCompleto}</td>
+                    <td className='px-6 py-4'>{item.fechaContrato}</td>
+                    <td className='px-6 py-4'>{item.departamento.nombre}</td>
+                    <td className='px-6 py-4'>${item.sueldo}</td>
+                    <td className='px-6 py-4 flex gap-2 justify-start'>
+                      <Link href="/" className='fill-red-600'>
+                        <Image src="/images/pen-to-square-solid.svg" width={25} height={25} alt='edit' title='close' className='fill-yellow-500' />
+                      </Link>
+                      <button className=''>
+                        <Image src="/images/trash-can-solid.svg" width={25} height={25} alt='edit' title='close' className='fill-yellow-500' />
+                      </button>
+                    </td>
+                  </tr>
 
-              ))
-            }
-          </tbody>
-        </table>
-
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
