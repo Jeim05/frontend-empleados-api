3f8517d69e57e5b63b0bd92b20986c7a8daebe17
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-08 23:09:01 -0600

    Update page.tsx

--- app/editarempleado/[id]/page.tsx
+++ app/editarempleado/[id]/page.tsx
@@ -5,7 +5,18 @@ import Swal from 'sweetalert2'
 import { appsettings } from '@/settings/appsettings'
 import Link from 'next/link'
 
+const initialEmpleado: Empleado = {
+  nombreCompleto: "",
+  departamento: {
+    idDepartamento: 0,
+    nombre: ""
+  },
+  sueldo: 0,
+  fechaContrato: ""
+}
+
 export default function EditarEmpleado() {
+
   return (
     <div>page</div>
   )
