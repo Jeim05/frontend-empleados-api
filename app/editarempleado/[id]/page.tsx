fed1072918d56d09be1e3b0b238fedb81f1b0ee5
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-08 23:08:27 -0600

    Update page.tsx

--- app/editarempleado/[id]/page.tsx
+++ app/editarempleado/[id]/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+"use client"
+import { useState, ChangeEvent, FormEvent, useEffect } from 'react'
+import { Empleado } from '@/Interfaces/Empleado'
+import Swal from 'sweetalert2'
+import { appsettings } from '@/settings/appsettings'
+import Link from 'next/link'
 
 export default function EditarEmpleado() {
   return (
