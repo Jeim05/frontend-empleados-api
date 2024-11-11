6177ceff55d774e133ee195338ad102853ee1cf7
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-10 19:24:28 -0600

    update url

--- app/editarempleado/[id]/page.tsx
+++ app/editarempleado/[id]/page.tsx
@@ -4,8 +4,11 @@ import { Empleado } from '@/Interfaces/Empleado'
 import Swal from 'sweetalert2'
 import { appsettings } from '@/settings/appsettings'
 import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import { Departamento } from '@/Interfaces/Departamento'
 
 const initialEmpleado: Empleado = {
+  idEmpleado: 0,
   nombreCompleto: "",
   departamento: {
     idDepartamento: 0,
@@ -16,6 +19,10 @@ const initialEmpleado: Empleado = {
 }
 
 export default function EditarEmpleado() {
+  const [empleado, setEmpleado] = useState<Empleado>(initialEmpleado);
+  const [departamentos, setDepartamentos] = useState<Departamento[]>([]);
+  const { id } = useParams();
+
 
   return (
     <div>page</div>
